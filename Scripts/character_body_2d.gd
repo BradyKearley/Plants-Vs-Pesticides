9b481f4d7b8f4bc978b655405c7efa08987ebd35
Brady Raye Kearley <bradykearley84@gmail.com>
2025-01-18 01:38:37 -0500

    Fixed Player

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = -400.0
 var canDash = true
 var dashSpeed = 1
 var dashCooldown
+func _ready() -> void:
+	$AnimatedSprite2D.play("Idle")
 func _process(delta: float) -> void:
 	var mouse_position = get_global_mouse_position()
 	look_at(mouse_position)
