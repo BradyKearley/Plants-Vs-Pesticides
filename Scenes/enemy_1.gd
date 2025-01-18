07bb6c0c8f89a45e4f6ef9991f19d8f56a71e0ff
1saacbz <156383637+1saacbz@users.noreply.github.com>
2025-01-18 01:57:18 -0500

    enemy
    stops 100 away from enemy

--- Scenes/enemy_1.gd
+++ Scenes/enemy_1.gd
@@ -2,18 +2,21 @@ extends CharacterBody2D
 
 var plant_speed = 200
 var player_detected = false
-var player = null
+var player: Node2D = null
+var stop_distance = 100  # Minimum distance to stop moving
 
 func _physics_process(delta):
-	if player_detected && player:
-		var direction = (player.position - position).normalized()
-		position += direction * plant_speed * delta
+	if player_detected and player:
+		var distance_to_player = position.distance_to(player.position)
+		if distance_to_player > stop_distance:
+			var direction = (player.position - position).normalized()
+			position += direction * plant_speed * delta
 
 func _on_detection_roots_body_entered(body: Node2D) -> void:
 	player_detected = true
 	player = body
- 
 
 func _on_detection_roots_body_exited(body: Node2D) -> void:
-	player_detected = true
-	player = null
+	if body == player:
+		player_detected = false
+		player = null
