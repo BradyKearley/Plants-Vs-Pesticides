8b0a590a130882215b263b79b392c8e1a385ba37
Brady Raye Kearley <bradykearley84@gmail.com>
2025-01-18 22:45:44 -0500

    WOrks?

--- Scenes/tree.gd
+++ Scenes/tree.gd
@@ -1,17 +1,18 @@
 extends CharacterBody2D
 @export var MrBust : PackedScene   
 @export var Treebullet : PackedScene   
+@export var Branch : PackedScene
 var player_detected = false
 var player: Node2D = null
 var stop_distance = 100  
-var health = 500
-const maxHealth = 500
-
+var health = 1000
+const maxHealth = 1000
+var phase = 1
 func _ready() -> void:
 	$AnimatedSprite2D.play("idle")
 	$AttackTimer.start()
 	$SpawnTimer.start()
-		
+	
 func _on_detection_roots_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		player_detected = true
@@ -26,6 +27,12 @@ func hit():
 	health -= 25
 	if health <= 0:
 		queue_free()
+	if health < 500 and phase == 1:
+		spawnGrout()
+		phase = 2
+	if health < 250 and phase == 2:
+		spawnGrout()
+		phase = 3
 func spawn():
 	var busterBus = MrBust.instantiate()
 	add_child(busterBus)
@@ -68,3 +75,8 @@ func _on_attack_timer_timeout() -> void:
 func _on_spawn_timer_timeout() -> void:
 	if player_detected:
 		spawn()
+func spawnGrout():
+	var Grout = Branch.instantiate()
+	add_child(Grout)
+	Grout.global_position = global_position
+	
